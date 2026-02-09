41cbb70ce7353ee895365f9ccd2ce38956877313
cmd-d4ksh <dakshonthego@gmail.com>
2026-02-09 11:39:57 +0530

    Integrated calculator features

--- Desktop/Experiment4-main/main.py
+++ Desktop/Experiment4-main/main.py
@@ -1,4 +1,17 @@
+from add import add
+from sub import sub
+from mul import mul
+
 def main():
+    a = 10
+    b = 5
+
+    print("Addition:", add(a, b))
+    print("Subtraction:", sub(a, b))
+    print("Multiplication:", mul(a, b))
+
+if __name__ == "__main__":
+    main()def main():
     print("Calculator app — features coming soon")
 
 if __name__ == "__main__":
